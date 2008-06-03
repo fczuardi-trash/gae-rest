08074b75375c4fddf82df4b3f6e1d691308fe5fb
Fabricio Zuardi <fabricio@Zuardi-main.local>
2008-06-03 03:36:34 -0700

    Correcting identation (i am a Python n00b)

--- gae-rest-query-tester.py
+++ gae-rest-query-tester.py
@@ -14,26 +14,26 @@ class GaeRestQueryTester(webapp.RequestHandler):
         self.response.out.write('$elems: %s\n' % repr(elems))
         self.response.out.write('$params: %s\n' % repr(params))
 
-    def param_parser(params):
-        if params is None or params == '': return []
-        return params.split('&')
+def param_parser(params):
+    if params is None or params == '': return []
+    return params.split('&')
 
-    def element_parser(elements):
-        hash = {}
-        elements = elements.split('/')
-        for element in elements:
-            name, params = re.findall('([^(]*)(?:\((.*)\))?', element)[0]
-            hash[name] = param_parser(params)
-        return hash
+def element_parser(elements):
+    hash = {}
+    elements = elements.split('/')
+    for element in elements:
+        name, params = re.findall('([^(]*)(?:\((.*)\))?', element)[0]
+        hash[name] = param_parser(params)
+    return hash
 
-    def query_parser(query):
-        query = urllib2.unquote(query)
-        elements, params = urllib2.splitquery(query) # foo?bar=1 becomes ('foo', 'bar=1')
-        return element_parser(elements), param_parser(params)
+def query_parser(query):
+    query = urllib2.unquote(query)
+    elements, params = urllib2.splitquery(query) # foo?bar=1 becomes ('foo', 'bar=1')
+    return element_parser(elements), param_parser(params)
 
-    def main():
-        application = webapp.WSGIApplication([('/xn/(.*)/(.*)/(.*)', GaeRestQueryTester)], debug=True)
-        wsgiref.handlers.CGIHandler().run(application)
+def main():
+    application = webapp.WSGIApplication([('/xn/(.*)/(.*)/(.*)', GaeRestQueryTester)], debug=True)
+    wsgiref.handlers.CGIHandler().run(application)
 
 if __name__ == "__main__":
     main()
