3801ec71c60220dd19393d88c83cb0b7f9698383
Jonas Galvez <jonas@codeazur.com.br>
2008-06-22 00:19:18 -0300

    ha, it works! basic introspection for models... :)

--- examples/cars/gae-rest/xn.py
+++ examples/cars/gae-rest/xn.py
@@ -18,43 +18,46 @@ def expose_model(name, cls):
 
 def expose(only=[]):
   loadedfiles = []
+  ignorelist = [os.path.splitext(os.path.split(f)[1])[0] for f in
+                glob.glob(os.path.join(os.path.abspath(os.path.dirname(__file__)), '*.py'))]
   for name, dirs, files in os.walk(os.path.join(__file__, '..', '..')):
     pyfiles = glob.glob('%s/*.py' % name)
     for pyfile in pyfiles:
       pydir, pyfile = os.path.split(pyfile)
       pyfile = os.path.splitext(pyfile)[0]
-      if pyfile in ('__init__', 'xn', 'xnquery'): continue
+      if pyfile in ignorelist: continue
       sys.path += [pydir]
       loadedfiles.append(__import__(pyfile))
       sys.path.remove(pydir)
   if len(only) > 0:
     for model in only:
-      if model in sandboxed_globals:
-        expose_model(model, sandboxed_globals['model'])
+      for lf in loadedfiles:
+        if model in dir(lf):
+          obj = getattr(lf, model)
+          if type(obj) == db.PropertiedClass:
+            expose_model(model, obj)
   else:
     for lf in loadedfiles:
       for name in dir(lf):
         obj = getattr(lf, name)
-        if type(obj) == type(db.PropertiedClass):
+        if type(obj) == db.PropertiedClass:
           expose_model(name, obj)
 
 expose()
 
 def retrieve_model_property_list(entity):
-  q = db.GqlQuery("SELECT * FROM XNExposedModel WHERE name = 'Car'")#:1", entity)
+  q = db.GqlQuery("SELECT * FROM XNExposedModel WHERE name = :1", entity)
   return q[0].model_properties
 
 class XNQueryHandler(webapp.RequestHandler):
   def get(self, format, version, query):
     xnquery = XNQueryParser(query, os.environ['QUERY_STRING'])
     self.response.headers['Content-Type'] = 'text/plain'
-    #self.response.out.write(repr(expose()))
-    #return
     gqlquery = GQLQueryBuilder(xnquery)
-    contents = db.GqlQuery(str(gqlquery))
-    for content in contents:
-      self.response.out.write('$content: %s\n' % repr(content))
-    self.response.out.write(retrieve_model_property_list(xnquery.resources.content.selectors.type.rightside))
+    objects = db.GqlQuery(str(gqlquery))
+    for object in objects:
+      self.response.out.write('$object[type=%s]: %s\n' % (xnquery.resources.content.selectors.type.rightside, repr(content)))
+    self.response.out.write('[debug] Entity property list: %s.' % repr(retrieve_model_property_list(xnquery.resources.content.selectors.type.rightside)))
 
 def main():
   application = webapp.WSGIApplication([('/xn/(.*)/(.*)/(.*)', XNQueryHandler)], debug=True)
