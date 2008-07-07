f4877316bdbb043fc217a66a29f64f114e934660
Fabricio Zuardi <fabricio@Zuardi-main.local>
2008-07-06 20:43:51 -0700

    minor, cleaning up the default config.py

--- config.py
+++ config.py
@@ -9,8 +9,9 @@ attributes to the equivalent atom elements.
 gae-rest needs to import the files containing your Models since it is 
 unable to dynamic infer what are all the available entity kinds 
 (content types) in your application
+Example: models = ['my_models.py', 'my_expandos.py']
 '''
-models = ['teh/blog.py', 'teh/utils.py', 'bloog/model.py']
+models = []
 
 '''
 which attribute on each Entity can be used as atom <author> element
